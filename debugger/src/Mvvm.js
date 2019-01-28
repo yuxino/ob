a492874de5bfbee4063c9581654119414763ad12
nbsaw <nbsawz@gmail.com>
2019-01-29 00:03:53 +0800

    update comment

--- debugger/src/Mvvm.js
+++ debugger/src/Mvvm.js
@@ -3,19 +3,18 @@ import Watcher from "./Watcher";
 
 class Mvvm {
   constructor(options = {}) {
-    // 简化了$options的处理
     this.$options = options;
-    // 简化了对data的处理
     let data = (this._data = this.$options.data);
-    // 将所有data最外层属性代理到Mvvm实例上
     Object.keys(data).forEach(key => this._proxy(key));
-    // 监听数据
     observe(data);
   }
-  // 对外暴露调用订阅者的接口，内部主要在指令中使用订阅者
+
+  // export function to outside
   $watch(expOrFn, cb) {
     new Watcher(this, expOrFn, cb);
   }
+
+  // proxy value in this._data
   _proxy(key) {
     Object.defineProperty(this, key, {
       configurable: true,
